befa6b0a01b24a51a20db95ca26e37c78df4073c
kingbarz <mrnwobodo@gmail.com>
2022-07-03 23:11:56 +0100

    second commit: I haved added the bouncing logic on both the walls and the paddle

--- ball.py
+++ ball.py
@@ -8,8 +8,16 @@ class Ball:
         self.ball.color("#BC4749")
         self.ball.shapesize(stretch_wid=0.6, stretch_len=0.6)
         self.ball.penup()
+        self.x_move = 1
+        self.y_move = 1
 
     def move(self):
-        new_x = self.ball.xcor() + 1
-        new_y = self.ball.ycor() + 1
+        new_x = self.ball.xcor() + self.x_move
+        new_y = self.ball.ycor() + self.y_move
         self.ball.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
--- main.py
+++ main.py
@@ -9,3 +9,15 @@ ball = Ball()
 while True:
     screen.mainscreen.update()
     ball.move()
+
+    #Detect Collision with Wall
+    if ball.ball.ycor() > 188:
+        ball.bounce_y()
+    elif ball.ball.xcor() > 230 or ball.ball.xcor() < -240:
+        ball.bounce_x()
+
+    #Detect collision with paddle
+    if ball.ball.distance(paddle.paddle) < 50 and ball.ball.ycor() < -167:
+        ball.bounce_y()
+
+
